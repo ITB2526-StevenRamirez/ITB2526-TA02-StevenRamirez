8c2282cc5cfee2bff2277e4a7377980a5e5ca9b1
ITB2526-Steven-Ramirez <steven.ramirez.7e7@itb.cat>
2025-09-23 12:46:32 +0200

    Commit de comprobadoredad.py

---
+++ comprobadoredad.py
@@ -0,0 +1,21 @@
+# programa que calcula si eres mayor o menor de edad.
+
+edad=int(input("Introduce tu edad "))
+# Se crea una variable a la cual le daremos un valor
+
+if edad < 18:
+    print("Menor de edad")
+else:
+    print("Mayor de edad")
+
+print("Fin del programa")
+
+# De linea 6 a 7 es un if que si introduces un numero menor a 18 te manda un print diciendo Menor de edad.
+# De linea 8 a 9 es un else, en este caso si has puesto 18 o mas te manda un print diciendo Mayor de edad.
+# Linea 11 indica con un print que hemos finalizado el proyecto.
+'''
+Steven Ramirez Trinidad
+23/09/2025
+ASIXc1A MDS DIG RA1-2
+Descripción: Calculadora edad
+'''
