a9747806e04204a1827d84f6aa5f6382418edc1b
ITB2526-Steven-Ramirez <steven.ramirez.7e7@itb.cat>
2025-09-23 12:31:08 +0200

    Commit de prueba1.py y holamundo.py

---
+++ holamundo.py
@@ -0,0 +1,11 @@
+# Esto es un código donde se hará un print con texto.
+
+print("¡Hola mundo!")
+# Este código hará que la consola imprima la frase ¡Hola mundo! en el terminal.
+
+'''
+Steven Ramirez Trinidad
+23/09/2025
+ASIXc1A MDS DIG RA1-2
+Descripción: Print
+'''
